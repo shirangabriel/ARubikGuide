59fa3fab7a8f2932db7874f0d54299010345a3de
Shiran Gabriel <gabriel.stcb@gmail.com>
2024-01-20 13:25:30 +0530

    Initial Commit

---
+++ ARubikGuide/ARubikGuideApp.swift
@@ -0,0 +1,17 @@
+//
+//  ARubikGuideApp.swift
+//  ARubikGuide
+//
+//  Created by Gabe on 2024-01-20.
+//
+
+import SwiftUI
+
+@main
+struct ARubikGuideApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
