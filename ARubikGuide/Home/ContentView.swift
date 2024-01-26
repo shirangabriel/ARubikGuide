bc31dffabeaa0a08f38402ede0a03f2d1e52a8a9
Shiran Gabriel <gabriel.stcb@gmail.com>
2024-01-26 20:18:01 +0530

    feat: handles navigation

--- ARubikGuide/Home/ContentView.swift
+++ ARubikGuide/Home/ContentView.swift
@@ -8,36 +8,17 @@
 import SwiftUI
 
 struct ContentView: View {
-    
-    
-    @State private var selectedCubeFaceIndex: Int?
-    @State private var navigateToColorMapping = false
-    @State private var path = NavigationPath()
-    
-    func handleOnCubeFacePressed(index: Int){
-        print("onPress")
-        
-        self.selectedCubeFaceIndex = index
-        
-        
-        if let _selectedCubeFaceIndex = self.selectedCubeFaceIndex {
-           // Trigger navigation only after selecting a face
-           self.navigateToColorMapping = true
-       }
-    }
-    
-    
     var body: some View {
+            Spacer()
             Text("Lets match the cube")
                 .font(.title)
                 .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
                 .frame(alignment: .topLeading)
                 .foregroundColor(.blue)
                 .multilineTextAlignment(.leading)
-            
-            
-        RubikFacesGrid(onCubeFacePress: handleOnCubeFacePressed)
+        
 
+            RubikFacesGrid()
     }
 }
 
--- ARubikGuide/Home/RubikFacesGrid.swift
+++ ARubikGuide/Home/RubikFacesGrid.swift
@@ -8,14 +8,6 @@
 import SwiftUI
 
 struct RubikFacesGrid: View {
-    
-    let onCubeFacePress: (Int) -> () // Declare the callback parameter
-    
-    init(onCubeFacePress: @escaping (Int) -> Void) {
-           self.onCubeFacePress = onCubeFacePress
-    }
-    
-    
     var body: some View {
         
         let firstHalf = rubiksCube.prefix(3)
@@ -24,44 +16,44 @@ struct RubikFacesGrid: View {
         let indicesFirstHalf = Array(0..<firstHalf.count)
         let indicesSecondHalf = Array(0..<secondHalf.count)
         
-    
-        
-        
-        HStack{
-            VStack {
-                ForEach(Array(zip(indicesFirstHalf, firstHalf)), id:\.0) { index, cube in
-                    Button(action: {
-                        // Access the index here
-                        print("index: \(index), cube: \(rubiksCube[index])")
-                        
-                        onCubeFacePress(index)
-
-                        
-                    }) {
-                        CubeSide(colors: cube)
-                            .padding(2)
+        NavigationSplitView {
+            HStack{
+                VStack {
+                    ForEach(Array(zip(indicesFirstHalf, firstHalf)), id:\.0) { index, cube in
+                        NavigationLink {
+                            ColorMapping()
+                        } label: {
+                            CubeSide(colors: cube)
+                                .padding(2)
+                        }
                     }
                 }
+                
+                VStack {
+                    ForEach(Array(zip(indicesSecondHalf, secondHalf)), id:\.0) { index, cube in
+                        
+    //                    print("index: \(firstHalf.count + index), cube: \(rubiksCube[firstHalf.count + index])")
+                        
+                        NavigationLink {
+                            ColorMapping()
+                        } label: {
+                            CubeSide(colors: cube)
+                                .padding(2)
+                        }
+                        
+                    }
+                }
+               
             }
             
-            VStack {
-                
-                ForEach(Array(zip(indicesSecondHalf, secondHalf)), id:\.0) { index, cube in
-                    Button(action: {
-                        // Access the index here
-                        print("index: \(firstHalf.count + index), cube: \(rubiksCube[firstHalf.count + index])")
-                        onCubeFacePress(firstHalf.count + index)
-                    }) {
-                        CubeSide(colors: cube)
-                            .padding(2)
-                    }
-                }
-            }
+        } detail: {
+            Text("Hey")
            
         }
+        
     }
 }
 
 #Preview {
-    RubikFacesGrid(onCubeFacePress: { _ in })
+    RubikFacesGrid()
 }
