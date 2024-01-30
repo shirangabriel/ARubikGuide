270748e9ecd40a816e54706c8c265ff5a82cbd56
Shiran Gabriel <gabriel.stcb@gmail.com>
2024-01-31 00:18:15 +0530

    Implements rubi cube with for 3d coordinates

--- CubeView/CubeView.swift
+++ CubeView/CubeView.swift
@@ -18,7 +18,7 @@ struct CubeView: View {
                 .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
         }
         
-    
+        
     }
     
     func createScene() -> SCNScene {
@@ -34,6 +34,7 @@ struct CubeView: View {
             }
         }
         
+        containerNode.eulerAngles = SCNVector3(GLKMathDegreesToRadians(40), GLKMathDegreesToRadians(0), 0)
         
         let cameraNode = SCNNode()
         cameraNode.camera = SCNCamera()
@@ -52,9 +53,9 @@ func cube(color: UIColor, x: Float, y: Float, z: Float) ->SCNNode {
     
     
     let box = SCNBox(width: squareSize, height: squareSize, length: squareSize, chamferRadius: chamferRadius)
-   
-    box.materials = [centerPiece(x: x, y: y, z: z)]
- 
+    
+    box.materials = getPiecesForCoordinates(x: x, y: y, z: z)
+    
     
     let node = SCNNode(geometry: box)
     node.position = SCNVector3(x:x, y: y, z: z)
@@ -62,30 +63,78 @@ func cube(color: UIColor, x: Float, y: Float, z: Float) ->SCNNode {
     return node
 }
 
-
-private func centerPiece(x: Float, y: Float, z: Float) -> SCNMaterial {
+private func getPiecesForCoordinates(x: Float, y: Float, z: Float) -> [SCNMaterial] {
     let key = "\(Int(x))\(Int(y))\(Int(z))"
- 
-    let materials: [String: (UIColor, Int)] = [
-        "001": (.green, 1), // front
-        "100": (.red, 2), // right
-        "00-1": (.blue, 3), // back
-        "-100": (.orange, 4), // left
-        "010": (.yellow, 5), // top
-        "0-10": (.white, 6), // bottom
-    ]
-     
-     
     
-    if let material = materials[key] {
-           return coloredMaterial(with: material.0, atIndex: material.1)
+    switch key {
+        
+        // Center Pieces
+        case "001": return face(front: UIColor.green)
+        case "100": return face(right: .red)
+        case "00-1": return face(back: .blue)
+        case "-100": return face(left: .orange)
+        case "010": return face(top: .yellow)
+        case "0-10": return face(bottom: .white)
+        
+        
+        // Corner Pieces
+        case "-111": return face(front: .green, left: .orange, top: .yellow)
+        case "-11-1": return face( back: .blue, left: .orange, top: .yellow)
+        case "-1-1-1": return face( back: .blue, left: .orange, bottom: .white)
+        
+        case "-1-11": return face(front: .green, left: .orange, bottom: .white)
+        
+        case "111": return face(front: .green, right: .red, top: .yellow)
+        case "1-11": return face(front: .green, right: .red, bottom: .white)
+        
+        case "11-1": return face(right: .red, back: .blue, top: .yellow)
+        case "1-1-1": return face(right: .red, back: .blue, bottom: .white)
+        
+        // Middle Pieces
+        // Front (green)
+        case "011": return face(front: UIColor.green, top: .yellow)
+        case "-101": return face(front: UIColor.green, left: .orange)
+        case "101": return face(front: UIColor.green, right: .red)
+        case "0-11": return face(front: UIColor.green, bottom: .white)
+        
+        // Right (red)
+        case "110": return face(right: .red, top: .yellow)
+        case "1-10": return face(right: .red, bottom: .white)
+        case "10-1": return face(right: .red, back: .blue)
+        
+        
+        case "-110": return face(left: .orange, top: .yellow)
+        case "-10-1": return face(back: .blue, left: .orange)
+        case "-1-10": return face(left: .orange, bottom: .white)
+        
+        case "0-1-1": return face(back: .blue, bottom: .white)
+        case "01-1": return face(back: .blue, top: .yellow)
+        
+        default: return [coloredMaterial(with: .white)]
     }
     
-    return coloredMaterial(with: .white, atIndex: 0)
 }
 
 
-private func coloredMaterial(with color: UIColor, atIndex index: Int) -> SCNMaterial {
+private func face(front: UIColor? = nil, right: UIColor? = nil, back: UIColor? = nil, left: UIColor? = nil, top: UIColor? = nil, bottom: UIColor? = nil) -> [SCNMaterial] {
+    
+    let defaultColor = UIColor.white
+    
+    let materials: [SCNMaterial] = [
+        coloredMaterial(with: (front != nil) ? front! : defaultColor), // front
+        coloredMaterial(with: (right != nil) ? right! : defaultColor), // right
+        coloredMaterial(with: (back != nil) ? back! : defaultColor), // back
+        coloredMaterial(with: (left != nil) ? left! : defaultColor), // left
+        coloredMaterial(with: (top != nil) ? top! : defaultColor), // top
+        coloredMaterial(with: (bottom != nil) ? bottom! : defaultColor), // bottom
+        
+    ]
+    
+    return materials
+}
+
+
+private func coloredMaterial(with color: UIColor) -> SCNMaterial {
     let material = SCNMaterial()
     material.diffuse.contents = color
     material.isDoubleSided = false // Make sure both sides of the face have the same color
@@ -103,7 +152,7 @@ struct SceneKitView: UIViewRepresentable {
         view.scene = scene
         view.autoenablesDefaultLighting = true
         view.allowsCameraControl = true
-        view.backgroundColor = UIColor.lightText
+        view.backgroundColor = UIColor.white
         return view
     }
     
