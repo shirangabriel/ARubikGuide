ad082dbf0ed3301ca629c283e8901188e4e4ac26
Shiran Gabriel <gabriel.stcb@gmail.com>
2024-02-03 20:35:54 +0530

    chore: code clean up

--- ARubikGuide/Cube.swift
+++ ARubikGuide/Cube.swift
@@ -45,60 +45,43 @@ struct Cube {
         let key = "\(Int(x))\(Int(y))\(Int(z))"
         
         switch key {
+            // Corners: UFL, UFR, UBL, UBR, DFL, DFR, DBL, DBR
+        case "-111": return face( up: .yellow, front: .green, left: .orange) // UFL
+        case "111": return face( up: .yellow, front: .green, right: .red) // UFR
+        case "-11-1": return face( up: .yellow, back: .blue, left: .orange) // UBL
+        case "11-1": return face(up: .yellow, right: .red, back: .blue) // UBR
+            
+        case "-1-11": return face(front: .green, left: .orange, down: .white)  // DFL
+        case "1-11": return face(front: .green, right: .red, down: .white)   // DFR
+        case "-1-1-1": return face( back: .blue, left: .orange, down: .white) // DBL
+        case "1-1-1": return face(right: .red, back: .blue, down: .white) // DBR
             
             
-            
-            
-            // Center Pieces
+            // Centers: U, F, R, B, L, D
+        case "010": return face(up: .yellow)
         case "001": return face(front: UIColor.green)
-            // Corner Pieces
-        case "-111": return face( up: .yellow, front: .green, left: .orange)
-        case "-1-11": return face(front: .green, left: .orange, down: .white)
-        case "111": return face( up: .yellow, front: .green, right: .red)
-        case "1-11": return face(front: .green, right: .red, down: .white)
-            
-            // Edge Pieces
-        case "011": return face(up: .yellow, front: UIColor.green )
-        case "-101": return face(front: UIColor.green, left: .orange)
-        case "101": return face(front: UIColor.green, right: .red)
-        case "0-11": return face(front: UIColor.green, down: .white)
-            
         case "100": return face(right: .red)
         case "00-1": return face(back: .blue)
         case "-100": return face(left: .orange)
-        case "010": return face(up: .yellow)
         case "0-10": return face(down: .white)
             
-            
-            // Corner Pieces
-        case "-11-1": return face( up: .yellow, back: .blue, left: .orange)
-        case "-1-1-1": return face( back: .blue, left: .orange, down: .white)
-            
-            
-        case "11-1": return face(up: .yellow, right: .red, back: .blue)
-        case "1-1-1": return face(right: .red, back: .blue, down: .white)
-            
-            // Middle Pieces
-            
-            
-            // Right (red)
-        case "110": return face(up: .yellow, right: .red)
-        case "1-10": return face(right: .red, down: .white)
-        case "10-1": return face(right: .red, back: .blue)
+            // Edges: UF, UR, UB, UL,  DF, DR,
+            // DB, DL, FR, FL, BR, BL
+        case "011": return face(up: .yellow, front: .green ) // UF
+        case "110": return face(up: .yellow, right: .red) // UR
+        case "01-1": return face( up: .yellow, back: .blue) // UB
+        case "-110": return face( up: .yellow, left: .orange) // UL
+        case "0-11": return face(front: .green, down: .white) // DF
+        case "1-10": return face(right: .red, down: .white) // DR
+        case "0-1-1": return face(back: .blue, down: .white) // DB
+        case "-1-10": return face(left: .orange, down: .white) // DL
+        case "101": return face(front: .green, right: .red) // FR
+        case "-10-1": return face(back: .blue, left: .orange) // FL
+        case "10-1": return face(right: .red, back: .blue) // BR
+        case "-101": return face(front: .green, left: .orange) //  BL
             
             
-            
-        // left  (orange)
-        case "-110": return face( up: .yellow, left: .orange)
-        case "-10-1": return face(back: .blue, left: .orange)
-        case "-1-10": return face(left: .orange, down: .white)
-            
-            
-            
-        case "0-1-1": return face(back: .blue, down: .white)
-        case "01-1": return face( up: .yellow, back: .blue)
-            
-        default: return [coloredMaterial(with: .white)]
+        default: return [coloredMaterial(with: .clear)]
         }
         
     }
@@ -109,11 +92,11 @@ struct Cube {
         let defaultColor = UIColor.black
         
         let materials: [SCNMaterial] = [
-            coloredMaterial(with: (up != nil) ? up! : defaultColor), // up
             coloredMaterial(with: (front != nil) ? front! : defaultColor), // front
             coloredMaterial(with: (right != nil) ? right! : defaultColor), // right
             coloredMaterial(with: (back != nil) ? back! : defaultColor), // back
             coloredMaterial(with: (left != nil) ? left! : defaultColor), // left
+            coloredMaterial(with: (up != nil) ? up! : defaultColor), // up
             coloredMaterial(with: (down != nil) ? down! : defaultColor), // down
         ]
         
