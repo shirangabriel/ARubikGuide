38bbaf7bb1e51822a8e9ded8d536e50bb506da02
Shiran Gabriel <gabriel.stcb@gmail.com>
2024-02-06 22:25:54 +0530

    feat: add state for cube

--- ARubikGuide/CubeView/CubeView.swift
+++ ARubikGuide/CubeView/CubeView.swift
@@ -11,11 +11,17 @@ import SceneKit
 struct CubeView: View {
     @State private var angle: Double = 0
     @GestureState private var dragAmount = CGSize.zero
+    @State private var scene: SCNScene
+    @State private var originalRotation: SCNMatrix4 = SCNMatrix4Identity
     
+    
+    init(){
+        _scene = State(initialValue: createScene())
+    }
     
     
     var body: some View {
-        var mScene: SCNScene = createScene();
+        
         
         VStack {
             
@@ -25,60 +31,77 @@ struct CubeView: View {
             
             
             GeometryReader { geometry in
-                SceneKitView(scene: mScene, size: geometry.size)
+                SceneKitView(scene: scene, size: geometry.size)
                     .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
             }
             
             Button(action: {
                 // Rotate
-                rotateFace(scene: mScene)
+                rotateFace(scene: scene)
                 
             }, label: {
-                Text(":Button")
+                Text("Rotate")
+            })
+            
+            
+            Button(action: {
+                printFaces(scene: scene)
+                
+            }, label: {
+                Text("Print")
+            })
+            
+            Button(action: {
+                scene = createScene()
+                
+            }, label: {
+                Text("Reset")
             })
             
         }
         
     }
-    
-    func createScene() -> SCNScene {
-        let scene = SCNScene()
-        for i in -1...1 {
-            for j in -1...1 {
-                for k in -1...1 {
-                    let cube = Cube.init(color: .white, x: CFloat(i), y: CFloat(j), z: CFloat(k))
-                        .getCubeSCN()
-                    
-                    let key = "\(Int(i))\(Int(j))\(Int(k))"
-                    
-                    let container = SCNNode()
-                    container.name = key
-                    container.addChildNode(cube)
-                    scene.rootNode.addChildNode(container)
-                }
+   
+}
+
+
+func createScene() -> SCNScene {
+    let scene = SCNScene()
+    for i in -1...1 {
+        for j in -1...1 {
+            for k in -1...1 {
+                let cube = Cube.init(color: .white, x: CFloat(i), y: CFloat(j), z: CFloat(k))
+                    .getCubeSCN()
+                
+                let key = "\(Int(i))\(Int(j))\(Int(k))"
+                
+                let container = SCNNode()
+                container.name = key
+                container.addChildNode(cube)
+                scene.rootNode.addChildNode(container)
             }
         }
-        
-        let cameraNode = SCNNode()
-        cameraNode.camera = SCNCamera()
-        cameraNode.position = SCNVector3(x: 0, y: 0, z: 10)
-        scene.rootNode.addChildNode(cameraNode)
-        return scene
-        
     }
+    
+    let cameraNode = SCNNode()
+    cameraNode.camera = SCNCamera()
+    cameraNode.position = SCNVector3(x: 0, y: 0, z: 10)
+    scene.rootNode.addChildNode(cameraNode)
+    return scene
+    
 }
 
 
 func rotateFace(scene: SCNScene) -> Void {
     for container in scene.rootNode.childNodes {
         if ["-111", "-1-11", "-101", "-100", "-11-1", "-1-1-1", "-110", "-10-1", "-1-10"].contains(container.name){ // first container
-            let rotationAction = SCNAction.rotateBy(x: .pi * 0.5, y: 0, z: 0, duration: 4)
+            let rotationAction = SCNAction.rotateBy(x: .pi * 0.5, y: 0, z: 0, duration: 1)
             container.runAction(rotationAction)
         }
 
 
-        let waitAction = SCNAction.wait(duration: 6)
-        let rotationAction = SCNAction.rotateBy(x: 0, y: 0, z: .pi * 0.5, duration: 4)
+        let waitAction = SCNAction.wait(duration: 1)
+        let rotationAction = SCNAction.rotateBy(x: 0, y: 0, z: .pi * 0.5, duration: 1)
         let sequenceAction = SCNAction.sequence([waitAction, rotationAction]) // Create a sequence of actions
 
 
@@ -88,6 +111,14 @@ func rotateFace(scene: SCNScene) -> Void {
     }
             
 }
+
+
+func printFaces(scene: SCNScene) -> Void {
+    for container in scene.rootNode.childNodes {
+        print("\(container.name)" )
+    }
+}
+
 
 func rotateFaceRight(face: SCNNode) -> Void {
     let rotationAction = SCNAction.rotateBy(x: 0, y: 0, z: .pi * 2, duration: 4)
